815da35f4c84c686394cd67fd03d15be9dfcddd2
yolkranou <yael.polanco@hotmail.com>
2025-03-07 14:27:46 +0100

    Fix errori, cambio gestione parte grafica main page

--- Code/GitFit/app/src/main/java/com/example/gitfit/MainPageActivity.java
+++ Code/GitFit/app/src/main/java/com/example/gitfit/MainPageActivity.java
@@ -1,17 +1,24 @@
 package com.example.gitfit;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
 
-import androidx.annotation.Nullable;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-public class MainPageActivity extends Activity {
+public class MainPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_page);
-        
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
