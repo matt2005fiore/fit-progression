bc2ced9f6df868595a61a755f00cbacb2aa4ed78
tolovar <alessio.longo@itsrizzoli.it>
2025-03-10 17:53:06 +0100

    man I do love me some calendar

--- Code/GitFit/app/src/main/java/com/example/gitfit/CalendarActivity.java
+++ Code/GitFit/app/src/main/java/com/example/gitfit/CalendarActivity.java
@@ -3,10 +3,11 @@ package com.example.gitfit;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class CalendarActivity extends Activity {
+public class CalendarActivity extends Activity{
     @Override
     protected void onCreate(Bundle SavedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.CalendarActivity);
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.calendar_activity);
     }
+
 }
