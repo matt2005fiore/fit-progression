b0bbb5bbd811113b9efb538b94212eacb12842f3
yolkranou <yael.polanco@hotmail.com>
2025-03-14 14:55:31 +0100

    Aggiunto wireframe Visualizzatore obiettivi

--- Code/GitFit/app/src/main/java/com/example/gitfit/CalendarActivity.java
+++ Code/GitFit/app/src/main/java/com/example/gitfit/CalendarActivity.java
@@ -1,13 +1,26 @@
 package com.example.gitfit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CalendarActivity extends Activity{
     @Override
     protected void onCreate(Bundle SavedInstanceState) {
         super.onCreate(SavedInstanceState);
-        setContentView(R.layout.calendar_activity);
+        setContentView(R.layout.activity_calendar);
+
+        Button goalsButton = (Button)findViewById(R.id.GoalsButton);
+
+        goalsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CalendarActivity.this, SettingsActivity.class));
+            }
+        });
+        }
     }
 
-}
+
