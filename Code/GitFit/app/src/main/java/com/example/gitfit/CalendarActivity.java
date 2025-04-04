aa1aea5cf1b6742c27639a68fc2c9407f8f934ad
tolovar <alessio.longo@itsrizzoli.it>
2025-04-04 15:03:07 +0200

    Intent su Calendar

--- Code/GitFit/app/src/main/java/com/example/gitfit/CalendarActivity.java
+++ Code/GitFit/app/src/main/java/com/example/gitfit/CalendarActivity.java
@@ -2,17 +2,19 @@ package com.example.gitfit;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.icu.util.GregorianCalendar;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.Button;
 
-public class CalendarActivity extends Activity{
+public class CalendarActivity extends Activity {
     @Override
     protected void onCreate(Bundle SavedInstanceState) {
         super.onCreate(SavedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        Button goalsButton = (Button)findViewById(R.id.GoalsButton);
+        Button goalsButton = (Button) findViewById(R.id.GoalsButton);
 
         goalsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,5 +22,22 @@ public class CalendarActivity extends Activity{
                 startActivity(new Intent(CalendarActivity.this, SettingsActivity.class));
             }
         });
-        }
+
+    // Intent per inserire gli esercizi svolti nel calendario
+        Intent intent = new Intent(Intent.ACTION_INSERT);
+        intent.setType("vnd.android.cursor.item/event");
+        intent.putExtra(CalendarContract.Events.TITLE, "Learn Android");
+        intent.putExtra(CalendarContract.Events.DESCRIPTION, "Download Examples");
+
+        // Impostare data e ora
+        GregorianCalendar calDate = new GregorianCalendar(2025, 01, 01);
+        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, calDate.getTimeInMillis());
+        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, calDate.getTimeInMillis());
+
+        // Oppure farla apparire come attività svolta quel giorno come intero
+        intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+
+         // Renderlo privato
+        intent.putExtra(CalendarContract.Events.ACCESS_LEVEL, CalendarContract.Events.ACCESS_PRIVATE);
     }
+}
