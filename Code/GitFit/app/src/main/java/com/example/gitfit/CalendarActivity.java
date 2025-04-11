1d1154a46823b196d4b0f570426eea756e8a371e
tolovar <alessio.longo@itsrizzoli.it>
2025-04-11 03:52:45 +0200

    Calendar final

--- Code/GitFit/app/src/main/java/com/example/gitfit/CalendarActivity.java
+++ Code/GitFit/app/src/main/java/com/example/gitfit/CalendarActivity.java
@@ -9,8 +9,80 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
-public class CalendarActivity extends Activity {
+public class CalendarActivity extends Activity implements CalendarAdapter.OnItemListener{
 
+    private TextView meseAnnoText;
+    private RecyclerView calendarRecyclerView;
+    private LocalDate selectedDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar)
+        initWidgets();
+        selectedDate = LocalDate.now();
+        setMonthView();
+    }
+
+    private void initWidgets() {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        meseAnnoText = findViewById(R.layout.meseAnno);
+    }
+
+    private void setMonthView() {
+        meseAnnoText.setText(meseAnnoFromDate(selectedDate));
+        ArrayList<String> giorniInUnMese = giorniInUnMeseArray(selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(giorniInUnMese, onItemListener:this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), spanCount:7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter)
+    }
+
+    private ArrayList<String> giorniInUnMeseArray(LocalDate date) {
+        ArrayList<String> giorniInUnMeseArray = new ArrayList<>();
+        AnnoMese annoMese = AnnoMese.from(date);
+        int giorniInUnMese = annoMese.lengthOfMonth();
+
+        LocalDate primoDelMese = selectedDate.withDayOfMonth(1);
+        int giornoDellaSettimana = primoDelMese.getGiornoDellaSettimana().getValue();
+
+        for(int i = 1; i <= 42; i++) {
+            if(i<= giornoDellaSettimana || i > giorniInUnMese + giornoDellaSettimana) {
+                giorniInUnMeseArray.add("");
+            }
+            else {
+                giorniInUnMeseArray.add(String.valueOf(i - giornoDellaSettimana));
+            }
+        }
+        return giorniInUnMeseArray;
+    }
+
+    private String meseAnnoFromDate(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    public void mesePrecedente(View view) {
+        selectedDate = selectedDate.minusMonths(1);
+        setMonthView();
+    }
+
+    public void meseSuccessivo(View view) {
+        selectedDate = selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    @Override
+    public void onItemClick(int position, String dayText) {
+        if(dayText.equals("")) {
+            String message = dayText + " " + meseAnnoFromDate(selectedDate);
+            Toast.makeText(context:this, message, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    /* Codice vecchio
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +96,16 @@ public class CalendarActivity extends Activity {
             @Override
             public void onSelectedDayChange(CalendarView view, int anno, int mese, int giornoDelMese) {
                 // Azione esempio: mostrami un toast con la data selezionata
+<<<<<<< Updated upstream
                 String date = giornoDelMese + "/" + (mese + 1) + "/" + anno;
                 date.setText(" " + date + " ");
+=======
+                String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+                Toast.makeText(CalendarActivity.this, date, Toast.LENGTH_SHORT).show();
+>>>>>>> Stashed changes
             }
         });
-    }
+    } */
     
         public void onAddExerciseClick(View view) {
         // Intent per aggiungere un esercizio al calendario
@@ -50,5 +127,6 @@ public class CalendarActivity extends Activity {
 
         startActivity(intent);
     }
+
 }
     
